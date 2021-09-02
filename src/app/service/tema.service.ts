bb1a3866dd9f77e11c6a0c25e0993e17970eb0ff
Clara Montanhez <claramontanhez@gmail.com>
2021-09-02 18:55:08 -0300

    tema put e delete

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -12,14 +12,26 @@ export class TemaService {
   constructor(private http: HttpClient) { }
 
   token = {
-    headers: new HttpHeaders().set('Authorization',environment.token)
+    headers: new HttpHeaders().set('Authorization', environment.token)
   }
 
-  getAllTema(): Observable<Tema[]>{
+  getAllTema(): Observable<Tema[]> {
     return this.http.get<Tema[]>('https://blogdaclara.herokuapp.com/tema', this.token) //pega o tema que declarei acima
+  }
+
+  gitByIdTema(id: number): Observable<Tema> {
+    return this.http.get<Tema>(`https://blogdaclara.herokuapp.com/tema/${id}`, this.token)
   }
 
   postTema(tema: Tema): Observable<Tema> {
     return this.http.post<Tema>('https://blogdaclara.herokuapp.com/tema', tema, this.token) //registra o tema nesse endpoint
+  }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://blogdaclara.herokuapp.com/tema', tema, this.token)
+  }
+
+  deleteTema(id:number) {
+    return this.http.delete(`https://blogdaclara.herokuapp.com/tema/${id}`, this.token)
   }
 }
