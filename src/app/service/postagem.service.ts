a031287886a3dd6e35aa8624855cf25e61a1d1fc
Clara Montanhez <claramontanhez@gmail.com>
2021-09-05 20:23:25 -0300

    implementação do put e delete de postagens

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -16,27 +16,27 @@ export class PostagemService {
   }
 
   getAllPostagens(): Observable<Postagem[]>{
-    return this.http.get<Postagem[]>('http://localhost:8080/postagens', this.token)
+    return this.http.get<Postagem[]>('https://blogdaclara.herokuapp.com/postagens', this.token)
   }
 
   getByIdPostagem(id: number): Observable<Postagem>{
-    return this.http.get<Postagem>(`http://localhost:8080/postagens/${id}`, this.token)
+    return this.http.get<Postagem>(`https://blogdaclara.herokuapp.com/postagens/${id}`, this.token)
   }
 
   getByTituloPostagem(titulo: string): Observable<Postagem[]>{
-    return this.http.get<Postagem[]>(`http://localhost:8080/postagens/titulo/${titulo}`, this.token)
+    return this.http.get<Postagem[]>(`https://blogdaclara.herokuapp.com/postagens/titulo/${titulo}`, this.token)
   }
 
   postPostagem(postagem: Postagem) : Observable<Postagem>{
-    return this.http.post<Postagem>('http://localhost:8080/postagens', postagem, this.token)
+    return this.http.post<Postagem>('https://blogdaclara.herokuapp.com/postagens', postagem, this.token)
   }
 
   putPostagem(postagem: Postagem): Observable<Postagem>{
-    return this.http.put<Postagem>('http://localhost:8080/postagens', postagem, this.token)
+    return this.http.put<Postagem>('https://blogdaclara.herokuapp.com/postagens', postagem, this.token)
   }
 
   deletePostagem(id: number){
-    return this.http.delete(`http://localhost:8080/postagens/${id}`, this.token)
+    return this.http.delete(`https://blogdaclara.herokuapp.com/postagens/${id}`, this.token)
   }
 
 }
