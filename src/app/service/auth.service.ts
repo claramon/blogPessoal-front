1f3d2cf5564e20d4edde51f976912e37433bcf14
Clara Montanhez <claramontanhez@gmail.com>
2021-09-05 19:11:32 -0300

    implementação do post e get de postagens

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -22,6 +22,10 @@ export class AuthService {
     return this.http.post<User>('https://blogdaclara.herokuapp.com/usuarios/cadastrar',user)
   }
 
+  getByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`https://blogdaclara.herokuapp.com/usuarios/${id}`)
+  }
+
   logado(){
     let ok: boolean = false
 
